f117aba38d03b46c4ad7951ef91637ae0f43c3b5
cferdinandi <chris@gomakethings.com>
2017-06-08 13:14:39 -0400

    Updates

--- src/js/main/fullWidth.js
+++ src/js/main/fullWidth.js
@@ -4,8 +4,10 @@
  * @description Make page full-width (no padding or centering)
  * @version 0.0.1
  * @author  Chris Ferdinandi
+ * @param {Boolean} hideH1  If true, hide the H1 header
+ * @param {Boolean} wide    If true, go wide instead of full width
  */
-var fullWidth = function ( hideH1 ) {
+var fullWidth = function ( hideH1, wide ) {
 
 	'use strict';
 
@@ -18,25 +20,29 @@ var fullWidth = function ( hideH1 ) {
 	var h1 = document.querySelector( 'h1.first' );
 
 	// Go full width
-	document.documentElement.classList.add( 'dom-full-width' );
+	if (wide) {
+		document.documentElement.classList.add( 'dom-wide' );
+	} else {
+		document.documentElement.classList.add( 'dom-full-width' );
+	}
 
 	// Wrap elements in container class
-	if ( meta ) {
-		wrapElem( meta, '<div class="container">{{content}}</div>' );
+	if (!wide) {
+		if ( meta ) {
+			wrapElem( meta, '<div class="container">{{content}}</div>' );
+		}
+		if ( edit ) {
+			wrapElem( edit.parentNode.parentNode, '<div class="container">{{content}}</div>' );
+		}
+		if ( h1 ) {
+			wrapElem( h1, '<div class="container">{{content}}</div>' );
+		}
 	}
-	if ( edit ) {
-		wrapElem( edit.parentNode.parentNode, '<div class="container">{{content}}</div>' );
-	}
-	if ( h1 ) {
 
-		// If enabled, hide the primary h1 element
-		if ( hideH1 ) {
+	// If enabled, hide the primary h1 element
+	if ( h1 && hideH1 ) {
 			h1.style.display = 'none';
 			h1.style.visibility = 'hidden';
-		}
-
-		wrapElem( h1, '<div class="container">{{content}}</div>' );
-
 	}
 
 };
