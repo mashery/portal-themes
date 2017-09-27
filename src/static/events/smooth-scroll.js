57de2f4296a64e0ded914404a3a9e73d84baac30
Chris Ferdinandi <cferdinandi@tibco.com>
2017-09-27 13:39:00 -0400

    Update how smooth scroll is initialized

--- src/static/events/smooth-scroll.js
+++ src/static/events/smooth-scroll.js
@@ -1,4 +1,4 @@
-var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('.category-page a[href*="#"], .category-docs a[href*="#"], .category-docs a[href*="#"], .category-blogall a[href*="#"], .category-blogsingle a[href*="#"]');
 window.addEventListener('portalBeforeRenderAjax', function removeSmoothScroll () {
 	scroll.destroy();
 	window.removeEventListener('portalBeforeRenderAjax', removeSmoothScroll, false);
